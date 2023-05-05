188578de63fd3cb8a29befcda0315c4b27d8ff4d
jonasvonarb <mail@jonasvonarb.ch>
2023-05-05 11:40:20 +0200

    added mobile version

--- src/components/home/HomeInfo.vue
+++ src/components/home/HomeInfo.vue
@@ -12,7 +12,7 @@
       />
     </div>
     <div class="hide" :class="'active'" />
-    <div class="header small">
+    <div v-if="isLargeScreen" class="header small">
       <div>
         titel <br />
         info
@@ -25,15 +25,30 @@
         <ArrowDown />
       </div>
     </div>
+    <div class="header small" v-else>
+      <div>
+        titel <br />
+        year <br />
+        info <br />
+        colab [] or Studio
+      </div>
+      <div class="arrow">
+        <ArrowDown />
+      </div>
+    </div>
     <div class="projects">
       <div class="subtitle small" v-html="mToH(`### Design, Concept & Code`)" />
       <template v-for="year in Object.keys(data)">
         <template v-for="l in data[year]">
           <div class="project" v-if="l.work === 'DCC'">
             <div class="title" v-html="mToH(l.link)" />
-            <div class="year" v-html="year.replace('year', '')" />
+            <div
+              class="year"
+              :class="!isLargeScreen ? 'dark' : ''"
+              v-html="year.replace('year', '')"
+            />
             <div v-html="mToH(l.content)" />
-            <div v-html="mToH(l.colab)" />
+            <div :class="!isLargeScreen ? 'dark' : ''" v-html="mToH(l.colab)" />
           </div>
         </template>
       </template>
@@ -42,9 +57,13 @@
         <template v-for="l in data[year]">
           <div class="project" v-if="l.work === 'C'">
             <div class="title" v-html="mToH(l.link)" />
-            <div class="year" v-html="year.replace('year', '')" />
+            <div
+              class="year"
+              :class="!isLargeScreen ? 'dark' : ''"
+              v-html="year.replace('year', '')"
+            />
             <div v-html="mToH(l.content)" />
-            <div v-html="mToH(l.colab)" />
+            <div :class="!isLargeScreen ? 'dark' : ''" v-html="mToH(l.colab)" />
           </div>
         </template>
       </template>
@@ -53,9 +72,13 @@
         <template v-for="l in data[year]">
           <div class="project" v-if="l.work === 'P'">
             <div class="title" v-html="mToH(l.link)" />
-            <div class="year" v-html="year.replace('year', '')" />
+            <div
+              class="year"
+              :class="!isLargeScreen ? 'dark' : ''"
+              v-html="year.replace('year', '')"
+            />
             <div v-html="mToH(l.content)" />
-            <div v-html="mToH(l.colab)" />
+            <div :class="!isLargeScreen ? 'dark' : ''" v-html="mToH(l.colab)" />
           </div>
         </template>
       </template>
@@ -67,8 +90,10 @@
 import Pill from "@/components/UI/Pill.vue";
 import { mToH } from "@/helper";
 import ArrowDown from "@/icons/ArrowDown.vue";
-
+import { useMediaQuery } from "@vueuse/core";
 import data from "@/assets/data";
+
+const isLargeScreen = useMediaQuery("(min-width: 600px)");
 </script>
 
 <style lang="stylus">
@@ -130,12 +155,12 @@ import data from "@/assets/data";
     background-color var(--anthracite)
     width calc(100%);
     height auto
-    min-height calc(100vh - 93vh - 48px)
+    min-height calc(100% - 93vh - 48px)
     overflow-x scroll
     position relative
     border solid var(--anthracite)
     border-radius 5px
-    margin-bottom calc(100vh + 24px)
+    margin-bottom calc(100% + 24px)
     // box-shadow: 0px 0px 10px 2px var(--anthracite);
     // -webkit-box-shadow: 0px 0px 10px 2px var(--anthracite);
     // -moz-box-shadow: 0px 0px 10px 2px var(--anthracite);
@@ -155,27 +180,40 @@ import data from "@/assets/data";
   .title
     font-family EverettBlack
     text-transform uppercase
+
 @media (max-width: 600px)
   .container
-    height 100vh;
-    height -moz-available;
-    height -webkit-fill-available;
-    height fill-available;
+    height 50%;
+    padding 6px
+    overflow-y hidden
+    .header
+      top calc(65vh - 70px)
+      width calc(100% - 40px)
+      grid-template-columns: 1fr;
+      margin 0 20px
+      padding 3px 0
+      z-index 0
     .info
       height 10%
+    .hide
+      right 4px
     .projects
-      height auto;
-      width calc(100% - 24px);
+      width 100%;
+      top 65vh
+      padding 0 10px
+      margin-bottom calc(65vh)
       .project
-        padding 12px
+        padding 3px 0
+        padding-bottom 60px
         display flex
         flex-wrap wrap
         border-top solid white
         div
           width 100%
-          border none
         .title
           padding-bottom 0
+        .dark
+          color rgb(150,150,150)
         .year
           width 50px
 </style>
